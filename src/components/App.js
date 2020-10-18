eb35ec36ca1ce27b2dc4c1e26e83be3c4151277a
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-10-18 22:32:30 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,9 @@ class App extends Component {
   render() {
     return (
       <>
-        <ol key="relatives">
+        <ol key="relativeList">
           {this.list.map((members, index) => (
-            <li key={`relativeListItem${index + 1}`}>{members}</li>
+            <li key={"relativeListItem" + (index + 1)}>members</li>
           ))}
         </ol>
       </>
