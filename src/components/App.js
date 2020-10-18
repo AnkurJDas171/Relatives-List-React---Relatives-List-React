011a1a5dcb0d71c114dfaced4bf40309425d8a4a
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-10-18 22:37:24 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 class App extends Component {
-  list = ["ankur", "jyoti", "das"];
   render() {
+    const list = ["ankur", "jyoti", "das"];
     return (
       <>
         <ol key="relativeList">
-          {this.list.map((members, index) => (
-            <li key={"relativeListItem" + (index + 1)}>members</li>
+          {list.map((members, index) => (
+            <li key={"relativeListItem" + (index + 1)}>{members}</li>
           ))}
         </ol>
       </>
